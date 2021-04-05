bb7f9d8eaca0e6831899ea4f1b97483581c21410
talararavikumar <35607415+talararavikumar@users.noreply.github.com>
2021-04-05 17:10:01 +0530

    updated to metareducer,local stoarge

--- src/app/app-state/index.ts
+++ src/app/app-state/index.ts
@@ -1,11 +1,14 @@
 import {
+  ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
-  createSelector
+  createSelector,
+  MetaReducer
 } from '@ngrx/store';
 import * as fromUser from './reducers/user.reducer'
 import * as fromCart from './reducers/cart.reducer';
 import * as fromProducts from './reducers/products.reducer'
+import { localStorageSync } from 'ngrx-store-localstorage';
 
 export interface AppState {
   user:fromUser.userState
@@ -13,12 +16,27 @@ export interface AppState {
   products:fromProducts.productSate
 }
 
-export const appreducer ={
+// export const appreducer ={
+//   user:fromUser.userreducer,
+//   cart:fromCart.cartreducer,
+//   products:fromProducts.productReducer
+// }
+
+export const appreducer: ActionReducerMap<AppState> = {
   user:fromUser.userreducer,
   cart:fromCart.cartreducer,
   products:fromProducts.productReducer
+};
+
+const reducerKeys = ['user', 'cart', 'products'];
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({keys: reducerKeys,  rehydrate: true})(reducer);
 }
+
+export const metaReducers: MetaReducer<AppState>[] = [localStorageSyncReducer];
  
+
+
 //export const getCartState = createFeatureSelector<fromCart.cartState>('cart');
 //export const getProductsState = createFeatureSelector<fromProducts.productSate>('products');
 
